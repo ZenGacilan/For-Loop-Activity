eadae7e2198d9c7d1b55f96239c001cfe6e9f5c2
ZenGacilan <potapetekau@gmail.com>
2024-10-07 17:06:03 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,9 @@
+// Print numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Print only even numbers from 1 to 10
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
